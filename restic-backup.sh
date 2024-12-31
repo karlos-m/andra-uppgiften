0b4172ec9c11ba6fdd996b984c99b0d959cb24a9
karlos <karlos.masoudi@chasacademy.se>
2024-12-31 20:00:26 +0000

    added expleine to use this file

--- restic-backup.sh
+++ restic-backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# home/karlos/remot-decument/ : is the path to directory which i will backup, you can choice your own
+# home/karlos/remot_decument/ : is the path to directory which i will backup, you can choice your own
 restic -p /home/karlos/restic-pasword-file -r sftp:karlos@192.168.56.101:/home/karlos/restic-backup/ backup /home/karlos/remote_decuments/
